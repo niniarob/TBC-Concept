d0fade01c2adf8d519c63581fc70a8724ead4a1f
nino <nino@ninos-MacBook-Air.local>
2024-07-20 14:45:30 +0400

    In this section, burger menu dropdown, menu styles and functions have been added

--- javascript/burger_menu.js
+++ javascript/burger_menu.js
@@ -1,8 +1,36 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const burgerMenu = document.querySelector('.burger_menu');
-  
-    burgerMenu.addEventListener('click', () => {
-      burgerMenu.classList.toggle('open');
-    });
-  });
-  
+    const header = document.querySelector('header');
+    const mobileMenu = document.querySelector(".header_menu_mobile_version");
+
+    const toggleMenu = () => {
+        burgerMenu.classList.toggle('open');
+        header.classList.toggle('header_bg_color');
+
+        if (mobileMenu.classList.contains('show')) {
+            mobileMenu.classList.remove('show');
+            setTimeout(() => {
+                mobileMenu.style.display = 'none';
+            }, 500);
+        } else {
+            mobileMenu.style.display = 'block';
+            setTimeout(() => {
+                mobileMenu.classList.add('show');
+            }, 10); 
+        }
+    };
+
+    const handleResize = () => {
+        if (window.innerWidth > 991) {
+            mobileMenu.style.display = "none";
+            mobileMenu.classList.remove('show');
+            burgerMenu.classList.remove('open');
+            header.classList.remove('header_bg_color');
+        }
+    };
+
+    burgerMenu.addEventListener('click', toggleMenu);
+    window.addEventListener('resize', handleResize);
+});
+
